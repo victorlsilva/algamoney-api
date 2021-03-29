ba0aa22d1102f56c9f76276bd0f7bae9be0fa2a3
Victor <victorls1993@gmail.com>
2021-03-28 22:28:52 -0300

    Implementando Logout

---
+++ src/main/java/com/algamoney/algamoneyapi/resource/TokenResource.java
@@ -0,0 +1,27 @@
+package com.algamoney.algamoneyapi.resource;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/tokens")
+public class TokenResource {
+
+    @DeleteMapping("/revoke")
+    public void revoke(HttpServletRequest request, HttpServletResponse response){
+        Cookie cookie = new Cookie("refreshToken", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath(request.getContextPath() + "/oauth/token");
+        cookie.setMaxAge(0);
+
+        response.addCookie(cookie);
+        response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+    }
+
+}
